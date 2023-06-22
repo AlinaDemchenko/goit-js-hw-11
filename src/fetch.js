fdfd218a93effcf87148d2cf709634b3cab19e6f
Alina Demchenko <demalina88@gmail.com>
2023-06-21 22:17:51 -0700

    11

--- src/fetch.js
+++ src/fetch.js
@@ -1,20 +1,21 @@
 import axios from "axios";
 
-export async function getPictures(tag){
+export async function getPictures(tag, page){
    const response = await axios.get(`https://pixabay.com/api/`, {
         params: {
             key: "37591930-7d732b17549968c758f6fdf27",
             image_type: "photo",
-            orientation: "vertical",
+            orientation: "horizontal",
             safesearch: true,
             q: tag,
             per_page: 40,
-            page: 1
+            page: page
         }
       });
       return response;
     }
 
+    
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,90 +1,117 @@
 import Notiflix from 'notiflix';
- //   Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.css';
 import { getPictures } from './fetch';
 
-
 Notiflix.Notify.init({
-    width: '700px',
-    position: 'left-top',
-    backOverlay: true,
-    backOverlayColor: 'rgba(20, 20, 20, 0.17)',
-    fontSize: '26px',
-    failure: {
-      background: 'rgb(47, 37, 85)',
-      notiflixIconColor: 'rgb(255, 255, 255)',
-    },
-  });
+  width: '500px',
+  position: 'center-center',
+  backOverlay: true,
+  backOverlayColor: 'rgba(0,0,0,0.5)',
+  fontSize: '15px',
+  timeout: 1000,
+  failure: {
+    textColor: 'rgb(33, 33, 33)',
+    background: '#fff',
+    notiflixIconColor: 'rgb(33, 33, 33)',
+  },
+});
 
 const refs = {
-form: document.querySelector("#search-form"),
-gallery: document.querySelector(".gallery"),
-button: document.querySelector(".button")
-}
+  form: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+  button: document.querySelector('.button'),
+  guard: document.querySelector('.guard'),
+};
+let page = 1;
+let lightbox;
 
-refs.button.setAttribute("disabled", true)
-
-refs.form.addEventListener("input", (evt)=>{
-  if (evt.currentTarget.searchQuery.value.length > 0){
-    refs.button.removeAttribute("disabled")
-  } else{
-    refs.button.setAttribute("disabled", true)  
+refs.button.setAttribute('disabled', true);
+refs.form.addEventListener('input', evt => {
+  if (evt.currentTarget.searchQuery.value.length > 0) {
+    refs.button.removeAttribute('disabled');
+  } else {
+    refs.button.setAttribute('disabled', true);
   }
-})
-refs.form.addEventListener("submit", handlerSubmit)
+});
+refs.form.addEventListener('submit', handlerSubmit);
 
-async function handlerSubmit(evt){
+async function handlerSubmit(evt) {
   evt.preventDefault();
-  const q = evt.currentTarget.elements.searchQuery.value;
-  try {const data = await getPictures(q)
-  refs.gallery.insertAdjacentHTML('beforeend', renderPics(data));
-  new SimpleLightbox('.gallery a', {captionsData: 'data-info',
-  captionDelay: 250,});}
-  catch {error => console.log("Sorry, there are no images matching your search query. Please try again.")}
+  const q = evt.currentTarget.elements.searchQuery.value.replace(" ","+");
+page = 1;
+  try {
+    const picsData = await getPictures(q, page);
+    if (picsData.data.hits.length === 0) {
+      throw new Error();
+    }
+    refs.gallery.innerHTML = renderPics(picsData);
+    observer.observe(refs.guard);
+ lightbox = new SimpleLightbox('.gallery a', {
+      captionsData: 'data-info',
+      captionDelay: 250,
+    });
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+  } catch (error) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
 }
 
 function renderPics(picsData) {
-  return picsData.data.hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
-   return `<div class="photo-card">
+  return picsData.data.hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
     <a href="${largeImageURL}">
-    <img src="${webformatURL}" alt="${tags}" data-info="🩵 ${likes} 👀 ${views} 💬 ${comments} ⏬️ ${downloads}"/>
+    <img src="${webformatURL}" alt="${tags}" data-info="🩵 ${likes}  💬 ${comments} ⏬️ ${downloads} 👀 ${views}"/>
     </a>
-  </div>`
-  }).join("");
+  </div>`;
+      }
+    )
+    .join('');
 }
 
+const options = {
+  root: null,
+  rootMargin: '500px',
+  threshold: 0,
+}
+ function loadMore(entries, observer) {
+  const q = refs.form.elements.searchQuery.value;
+  entries.forEach(async (entry) => {
+      if (entry.isIntersecting) {
+        page += 1;
+          const picsData = await getPictures(q, page);
+          if (picsData.data.hits.length === 0) {
+            observer.unobserve(refs.guard);
+          }
+          refs.gallery.insertAdjacentHTML("beforeend", renderPics(picsData))
+          lightbox.refresh()
+          const { height: cardHeight } = document
+            .querySelector('.gallery')
+            .firstElementChild.getBoundingClientRect();
+          window.scrollBy({
+            top: cardHeight * 2,
+            behavior: 'smooth',
+          });
+        } 
+      })}
 
+const observer = new IntersectionObserver(loadMore, options);
 
-
-{/* <div class="info">
-<p class="info-item">
-  <b>🩵 ${likes}</b>
-</p>
-<p class="info-item">
-  <b>👀 ${views}</b>
-</p>
-<p class="info-item">
-  <b>💬 ${comments}</b>
-</p>
-<p class="info-item">
-  <b>⏬️ ${downloads}</b>
-</p>
-</div> */}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
